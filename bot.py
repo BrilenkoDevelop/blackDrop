201e5f7135dd83df6145cce72b30e9b2b105fa95
BrilenkoDevelop <74966604+BrilenkoDevelop@users.noreply.github.com>
2020-11-24 15:56:09 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,3 +3,9 @@ from telebot import types
 
 bot_token = "1377056545:AAEygm1eKZ37bofir_icosP_OguzzzabPek"
 bot = telebot.TeleBot(bot_token)
+
+@bot.message_handler(commands=["start"])
+def start_comand(message):
+    bot.send_message(message.from_user.id, "Приветсвие")
+ 
+bot.polling()
